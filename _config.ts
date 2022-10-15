ccad94da8a7edda14715f81520eb9ecb8fd5bcfc
Eddie Cheng <schaffenneu47@gmail.com>
2022-10-15 22:08:43 +0900

    add a few of things

--- _config.ts
+++ _config.ts
@@ -1,10 +1,12 @@
 import lume from "lume/mod.ts";
 import code_highlight from "lume/plugins/code_highlight.ts";
 import sass from "lume/plugins/sass.ts";
+import date from "lume/plugins/date.ts";
 
 const site = lume();
 
 site.use(code_highlight());
 site.use(sass());
+site.use(date());
 
 export default site;
---
+++ about.md
@@ -0,0 +1,9 @@
+---
+layout: layouts/about.njk
+title: About Me
+menu:
+  visible: true
+  order: 2
+---
+
+# TO BE CONTINUED
