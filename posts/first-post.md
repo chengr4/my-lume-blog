ce78ed93340bb4591bc624ea0f6e59f5dcb9a02b
Eddie Cheng <schaffenneu47@gmail.com>
2023-01-20 00:29:59 +0900

    archive

---
+++ posts/first-post.md
@@ -0,0 +1,13 @@
+---
+layout: layout.tsx
+title: test
+type: posts
+---
+
+# Hello World
+
+This is a test post.
+
+```javsscript
+const a = 1;
+```
--- search.tsx
+++ search.tsx
@@ -1,9 +1,9 @@
 export const title = "Welcome to my page";
 export const layout = "layout.tsx";
 
-export default (data, { search }) => (
+export default (data) => (
   <>
-    <h1>{data.title}</h1>
+  
     <p>This is my first post using lume. I hope you like it!</p>
     
   </>
